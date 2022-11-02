cfe5c84a9cd87d3265ba034da7d0f38f14c8c960
edo461 <117011930+edo461@users.noreply.github.com>
2022-11-02 22:13:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const butt_one = document.querySelector('.box_1');
+const butt_two = document.querySelector('.box_2');
+const butt_three = document.querySelector('.box_3');
+
+butt_one.addEventListener('click', (e) => {
+    const target = e.target
+    console.log(target);
+})
+
+butt_two.addEventListener('click', () => {
+    
+})
+
+butt_three.addEventListener('click', () => {
+    
+})
