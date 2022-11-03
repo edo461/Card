7d64e4b306236e5af9f59aaf88a6fb24f745b7d9
edo461 <117011930+edo461@users.noreply.github.com>
2022-11-03 22:02:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const butt_one = document.querySelector('.box_1');
 const butt_two = document.querySelector('.box_2');
 const butt_three = document.querySelector('.box_3');
+const button = document.querySelector('.button');
+const boxes = document.querySelector('.boxes');
 
 butt_one.addEventListener('click', (e) => {
     const target = e.target
     console.log(target);
-})
+});
 
-butt_two.addEventListener('click', () => {
-    
-})
+butt_two.addEventListener('click', (a) => {
+    const target = a.target
+    console.log(target);
+});
 
-butt_three.addEventListener('click', () => {
-    
-})
+butt_three.addEventListener('click', (b) => {
+    const target = b.target
+    console.log(target);
+});
+
+button.addEventListener('click', () => {
+    if (boxes.checked == true) 
+    return;
+});
